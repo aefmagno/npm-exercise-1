093a40d725f0c4912f65d3b89aacd84dc1a27d22
Aaron Magno <aefmagno@gmaik.com>
2022-10-06 07:06:25 +0800

    vcard qr generation

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const qr = require('qrcode-terminal');
 
 console.log(qr.generate('WIFI:S:SOCIALDESK - 9th Floor;T:WPA2;P:SocialD3$K09F;'));
+
+const vcard = `BEGIN:VCARD
+VERSION:4.0
+N:Aaron Magno
+EMAIL;type=WORK:aefmagno@gmail.com
+TEL;type=phone:+639054293375
+END:VCARD`;
+
+console.log(qr.generate(vcard));
