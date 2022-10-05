d1c03847818d16330348b62b63302bea886647c7
Aaron Magno <aefmagno@gmaik.com>
2022-10-06 07:28:36 +0800

    vcard qr generation

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ console.log(qr.generate('WIFI:S:SOCIALDESK - 9th Floor;T:WPA2;P:SocialD3$K09F;')
 const vcard = `BEGIN:VCARD
 VERSION:4.0
 N:Aaron Magno
-EMAIL;type=WORK:aefmagno@gmail.com
+EMAIL;type=WORK:aaron.magno@siteminder.com
 TEL;type=phone:+639054293375
 END:VCARD`;
 
