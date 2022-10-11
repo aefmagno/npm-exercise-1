19fd2abf0dad403270fc720ff1ae67564fb9563a
Aaron Magno <aefmagno@gmaik.com>
2022-10-11 09:21:38 +0800

    added prettier linter

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const qr = require('qrcode-terminal');
 
-console.log(qr.generate('WIFI:S:SOCIALDESK - 9th Floor;T:WPA2;P:SocialD3$K09F;'));
+console.log(
+  qr.generate('WIFI:S:SOCIALDESK - 9th Floor;T:WPA2;P:SocialD3$K09F;')
+);
 
 const vcard = `BEGIN:VCARD
 VERSION:4.0
