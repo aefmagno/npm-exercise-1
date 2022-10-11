e0751429b236585a5eb72316957a496256c723a1
Aaron Magno <77775186+aefmagno@users.noreply.github.com>
2022-10-11 09:29:21 +0800

    vcard qr generation (#4)
    vcard qr generation with prettier

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const qr = require('qrcode-terminal');
 
-console.log(qr.generate('WIFI:S:SOCIALDESK - 9th Floor;T:WPA2;P:SocialD3$K09F;'));
+console.log(
+  qr.generate('WIFI:S:SOCIALDESK - 9th Floor;T:WPA2;P:SocialD3$K09F;')
+);
+
+const vcard = `BEGIN:VCARD
+VERSION:4.0
+N:Aaron Magno
+EMAIL;type=WORK:aaron.magno@siteminder.com
+TEL;type=phone:+639054293375
+END:VCARD`;
+
+console.log(qr.generate(vcard));
